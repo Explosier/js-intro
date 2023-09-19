61bc32bf30c358adf8d5f9b2173fc923e07a8abc
Tadas Valantonis <valantonis1@gmail.com>
2023-09-19 11:14:19 +0300

    numbers

---
+++ kintamieji/number.js
@@ -0,0 +1,20 @@
+/*
+NUMBER
+
+Iniciavymo budas:
+- const (default)
+- let (jei kinta)
+- var (niekada)
+*/
+
+const amzius = 99;
+let pinigine = 0;
+var minusas = -7.56;
+
+const x1 = 1;
+const x2 = 2;
+const x3 = 3;
+const x4 = 4;
+const x5 = 5;
+
+console.log(x5);
