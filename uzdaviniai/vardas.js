b290c2675d1d96e05a9014d11b65faddae7aea9e
Tadas Valantonis <valantonis1@gmail.com>
2023-10-03 14:47:19 +0300

    name whitelist

--- uzdaviniai/vardas.js
+++ uzdaviniai/vardas.js
@@ -16,13 +16,39 @@ function arVardas(tekstas){
     for(let i=0; i<neleistiniSimboliai.length; i++){
         simbolis = neleistiniSimboliai[i];
         if(tekstas.includes(simbolis)){
-            return `Klaida: "${simbolis} yra neleistinas simbolis..`;
+            return `Klaida: "${simbolis}" yra neleistinas simbolis..`;
         }
     }
 
     return true;
 }
 
+// Namu darbai
+function arVardas2(tekstas){
+    if(tekstas[0] !== tekstas[0].toUpperCase()){
+        return `Klaida: "${tekstas}" vardas turi prasideti is didziosios raides..`;
+    }
+    if(tekstas.slice(1) !== tekstas.slice(1).toLowerCase()){
+        return `Klaida: "${tekstas}" vardas negali tureti daugiau didziuju raidziu negu pirmoji didzioji raide..`;
+    }
+    if(tekstas.split(' ').length > 1) {
+        return  `Klaida: "${tekstas}" varde negali buti tarpu..`;
+    }
+    const leistiniSimboliai = 'AaĄąBbCcČčDdEeĘęĖėFfGgHhIiĮįYyJjKkLlMmNnOoPpRrSsŠšTtUuŲųŪūVvZzŽž';
+    for(let i=0; i<tekstas.length; i++){
+        tekstoSimbolis = tekstas[i];
+        if(!leistiniSimboliai.includes(tekstoSimbolis)){
+            return `Klaida: Varde "${tekstas}" yra neleistinas simbolis "${tekstoSimbolis}"`;
+        }
+    }
+    return `Vardas: "${tekstas}" yra parasytas gerai..`;
+}
 
 
-console.log(arVardas('Labas'));
+
+console.log(arVardas2('Labas kebabas'));
+console.log(arVardas2('labas'));
+console.log(arVardas2('LabAS'));
+console.log(arVardas2('Laba8'));
+console.log(arVardas2('Laba"'));
+console.log(arVardas2('Labas'));
